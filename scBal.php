01c621eebe5124ffe5070c336553a5b6ccaf14a0
m4rthiz <m4rthiz@gmail.com>
2017-10-22 17:01:56 +0200

    add title pages

--- scBal.php
+++ scBal.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello Ulkit!</title>
+    <title>Bals Folk - Cie Foulbaz'Art</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/css/uikit.min.css"/>
     <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans|Raleway|Lakki+Reddy" rel="stylesheet">
